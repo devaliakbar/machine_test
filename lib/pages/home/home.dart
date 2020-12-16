0a80586b6613653216ce50010d06976d4fbedda8
Ali <dev.aliakbar@gmail.com>
2020-12-16 19:59:40 +0530

    Refactored

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -7,6 +7,8 @@ import 'package:machine_test/pages/home/widgets/partials/build_drawer.dart';
 import 'package:machine_test/pages/home/widgets/views/home_body.dart';
 import 'package:machine_test/pages/home/widgets/views/home_error.dart';
 import 'package:machine_test/pages/home/widgets/views/home_loading.dart';
+import 'package:machine_test/services/settings/app_theme.dart';
+import 'package:machine_test/services/settings/status_bar_color.dart';
 
 class Home extends StatefulWidget {
   static const String myRoute = '/home';
@@ -20,6 +22,10 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
+
+    StatusBarColor.setUpStatusbarColor(
+        backgroundColor: AppTheme.primaryGreenColor, whiteColor: true);
+
     BlocProvider.of<HomeBloc>(context).add(HomeLoadEvent());
   }
 
